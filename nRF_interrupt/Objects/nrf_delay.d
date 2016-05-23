da1d8842cb58a79d7dda576e20a35c28e92674a3
pgrecords <modevin@yandex.ru>
2016-05-23 11:25:32 +0300

    Add example for nRF51822 interrupt

---
+++ nRF_interrupt/Objects/nrf_delay.d
@@ -0,0 +1,14 @@
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_Drivers\4.0.0-2.alpha\delay\nrf_delay.c
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\compiler_abstraction.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\nrf.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\nrf51.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cm0.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cminstr.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\cmsis_armcc.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\ARM\CMSIS\5.0.0-Beta4\CMSIS\Include\core_cmfunc.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\system_nrf51.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\nrf51_bitfields.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.6.0\Device\Include\nrf51_deprecated.h
+.\objects\nrf_delay.o: E:\Software\Keil\ARM\PACK\NordicSemiconductor\nRF_Drivers\4.0.0-2.alpha\delay\nrf_delay.h
---
+++ nRF_interrupt/main.c
@@ -0,0 +1,38 @@
+#include <stdbool.h>
+#include "nrf.h"
+#include "nrf_gpio.h"
+
+
+bool state = 0;
+uint16_t i = 0;
+
+
+
+void GPIOTE_IRQHandler(void){
+	if(NRF_GPIOTE->EVENTS_IN[0] == 1) NRF_GPIOTE->EVENTS_IN[0] = 0;   // Clear every interrupt-event!!!
+	
+	//nrf_gpio_pin_set(29);
+	//nrf_gpio_pin_clear(29);
+	nrf_gpio_pin_toggle(29);
+	state = !state;
+	++i;
+	//nrf_gpio_pin_write(29, state);
+}
+
+
+
+int main(void){
+	nrf_gpio_cfg_input(1, NRF_GPIO_PIN_PULLUP);
+	nrf_gpio_cfg_output(29);
+	//nrf_gpio_pin_write(29, 1);
+	NVIC_EnableIRQ(GPIOTE_IRQn);
+	NRF_GPIOTE->CONFIG[0] =  (GPIOTE_CONFIG_POLARITY_HiToLo << GPIOTE_CONFIG_POLARITY_Pos)
+                   | (1 << GPIOTE_CONFIG_PSEL_Pos)
+                   | (GPIOTE_CONFIG_MODE_Event << GPIOTE_CONFIG_MODE_Pos);
+	NRF_GPIOTE->INTENSET = GPIOTE_INTENSET_IN0_Enabled << GPIOTE_INTENSET_IN0_Pos;
+	
+	while(1){
+		
+	}
+}
+
